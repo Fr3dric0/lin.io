f4fb598228ffa2900476ffe7b0cd80cb7aead31e
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-03-16 21:28:09 +0100

    Set /api route to 404

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.status(200).json({ msg: "hello world" })
-});
 
 
 router.all('*', (req, res, next) => {
